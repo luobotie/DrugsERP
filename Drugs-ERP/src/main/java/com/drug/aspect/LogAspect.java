0370e60b06a9806cbeca648135139498a27b4914
djl_first <781800530@qq.com>
2019-10-16 17:42:03 +0800

    回归

--- Drugs-ERP/src/main/java/com/drug/aspect/LogAspect.java
+++ Drugs-ERP/src/main/java/com/drug/aspect/LogAspect.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 public class LogAspect {
 	//前置通知：调业务逻辑之前
    public  void before(JoinPoint joinPoint){
-	   //类路径：调用的哪个类。
+	   //类路径：调用的哪个类。。
 	   String className=joinPoint.getTarget().getClass().getName();
 	   //调用了那个方法：方法名称
 	   String methodName=joinPoint.getSignature().getName();
